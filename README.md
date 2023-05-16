ccde64bc096f0992616475fedfbdf80da8e9fa47
navid <navidrahman92@gmail.com>
2023-05-16 19:36:50 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mi_card
+
+This project is a Personal profile page of mine.
+This is just a basic implementation of using flutter and first step towards diving more deep into this flutter development world.
+This Apps just shows my personal information (Name, Occupation, Mobile Number and Email).
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/Navid.jpg'),
+              ),
+              Text(
+                'Navid Rahman',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Color.fromARGB(255, 189, 254, 249),
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Color.fromARGB(255, 174, 242, 236),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+880 1828080005',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 26, 74, 69),
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'navidrahman92@gmail.com',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 26, 74, 69),
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
